447950512eb888733e20b7685cacef30531ed4b6
caio6644 <detvoid343@gmail.com>
2026-08-26 14:46:44 -0300

    meu primeiro repositorio

---
+++ astra16v/src/main/java/br/com/senac/df/astra16v/Astra16v.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.df.astra16v;
+
+/**
+ *
+ * @author caio61567586
+ */
+public class Astra16v {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
